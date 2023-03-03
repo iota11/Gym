66faf6f91d35be1a7be5d9f89b6ff9acfea2b764
iota11 <falconhe511@gmail.com>
2023-03-03 01:26:57 -0500

    add brake in horizontal movement

--- Assets/HeliTranslate.cs
+++ Assets/HeliTranslate.cs
@@ -4,10 +4,22 @@ using Unity.VisualScripting;
 using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
+
+
 public class HeliTranslate : MonoBehaviour
 {
-    
-
+    public enum FlyState
+    {
+        Normal,
+        Climb,
+        Stay,
+        Drop
+    };
+    public enum AccState {
+        SpeedUp,
+        SpeedDown,
+        Stable,
+    }
     private Transform m_transform;
     public Transform body_transform;
     public Transform des_transfrom;
@@ -20,12 +32,16 @@ public class HeliTranslate : MonoBehaviour
     public Vector3 acc_measure = new Vector3(0,0,0);
     public float noiseScale = 0.0f;
     public Animator anim;
+    public FlyState m_flyState;
+    public AccState m_accState;
 
     //
     private FastNoise _fastNoise;
     private float collectedTime;
     public float cur_speed_hori = 0.0f;
+    public Vector3 cur_vel_hori;
     public float cur_speed_ver = 0.0f;
+    public Vector3 cur_vel_ver;
     public float cur_rotateDeg = 0.0f;
     public float cur_rotateDeg2 = 0.0f;
     public float playSpeed = 0f;
@@ -34,7 +50,6 @@ public class HeliTranslate : MonoBehaviour
     public float last_speed_ver = 0.0f;
     public Vector3 accRotate;
     private Quaternion initial_body_rotation;
-    private Vector3 curPos;
 
     void Start()
     {
@@ -42,18 +57,13 @@ public class HeliTranslate : MonoBehaviour
         m_transform = GetComponent<Transform>();
         _fastNoise = new FastNoise();
         initial_body_rotation = body_transform.localRotation;
+        m_flyState = FlyState.Stay;
+        m_accState = AccState.Stable;
     }
     void FixedUpdate()
     {
-        curPos = m_transform.position;
-        if (!LocomoteHorizontal())
-        {
-            cur_speed_hori = 0.0f;
-        }
-        if(!LocomoteVertical())
-        {
-            cur_speed_ver = 0.0f;
-        }
+        LocomoteHorizontal();
+        LocomoteVertical();
         Veer();
         //ApplyNoise();
 
@@ -66,27 +76,30 @@ public class HeliTranslate : MonoBehaviour
     {
         
         Vector3 acc2D = new Vector3(acc_measure.x, 0f, acc_measure.z);
-        //Vector3 rotateAis = Vector3.Cross(m_transform.forward, m_transform.up).normalized;
-        Vector3 accWind = cur_speed_hori * 0.04f *m_transform.forward;
+        Vector3 accWind = cur_speed_hori * 0.02f *m_transform.forward;
         Vector3 acc_locomote = accWind + acc2D;
+
+        //locomotion acc rotation
         float rotateDeg = Mathf.Clamp(acc_locomote.magnitude * acc_sensitive, -45, 45);
         if(Vector3.Dot(acc2D, m_transform.forward) < 0)
         {
             rotateDeg = -rotateDeg; 
         }
+        
         if (Mathf.Abs(cur_rotateDeg - rotateDeg) > 0.5f)
         {
-            cur_rotateDeg = Mathf.Lerp(cur_rotateDeg, rotateDeg, 0.1f);
+            cur_rotateDeg = Mathf.Lerp(cur_rotateDeg, rotateDeg, 0.2f);
         }
+        //cur_rotateDeg = rotateDeg;
         Quaternion detlaRotationX = Quaternion.AngleAxis(cur_rotateDeg, new Vector3(1,0,0));
 
-        //rotation
+
+        //veer acc rotation
         float rotateDeg2 = Mathf.Clamp(accRotate.magnitude*-5 * acc_sensitive, -65, 65);
         if (Vector3.Dot(accRotate, m_transform.right) < 0)
         {
             rotateDeg2 = -rotateDeg2;
         }
-
         Quaternion detlaRotationZ = Quaternion.AngleAxis(cur_rotateDeg2, new Vector3(0, 0, 1));
         if (Mathf.Abs(cur_rotateDeg2 - rotateDeg2) > 0.5f)
         {
@@ -94,8 +107,11 @@ public class HeliTranslate : MonoBehaviour
         }
         body_transform.localRotation =  initial_body_rotation * detlaRotationZ * detlaRotationX;
 
-
-        playSpeed = Mathf.Clamp( Mathf.Abs(cur_rotateDeg2*0.6f + cur_rotateDeg) / 110f + 1f, 0f, 10f);
+        //apply playspeed
+        playSpeed = Mathf.Clamp( Mathf.Abs(Mathf.Abs(cur_rotateDeg2)*0.6f + Mathf.Abs(cur_rotateDeg)) / 80f + 1f, 0f, 10f);
+        if(m_accState == AccState.SpeedDown) {
+            playSpeed += 0.3f;
+        }
         anim.SetFloat( "flySpeed", playSpeed);
     }
 
@@ -121,22 +137,37 @@ public class HeliTranslate : MonoBehaviour
         last_speed_hori = cur_speed_hori;
         last_speed_ver = cur_speed_ver;
     }
-    private bool LocomoteHorizontal()
+    private void LocomoteHorizontal()
     {
         Vector3 distance = des_transfrom.position - m_transform.position;
         distance.y = 0;
         if (distance.magnitude > stopDis)
         {
-            if (cur_speed_hori < move_speed_hori)
-            {
-                cur_speed_hori += acc * Time.deltaTime;
+            if (distance.magnitude < stopDis * 10f) {
+                if (cur_speed_hori > 3f) {    //slow down to 3f
+                    cur_speed_hori -= acc *3* Time.deltaTime;
+                    m_accState = AccState.SpeedDown;
+                    m_transform.position += new Vector3(0, 1, 0) * 4f * Time.deltaTime;
+                    Debug.Log("brace");
+                } else {
+                    m_accState = AccState.Stable;
+                }
+            } else {
+                if (cur_speed_hori < move_speed_hori) { //acc to move speed max
+                    cur_speed_hori += acc  * Time.deltaTime;
+                    m_accState = AccState.SpeedUp;
+                    m_transform.position += new Vector3(0,1,0) *-10f * Time.deltaTime;
+                } else {
+                    m_accState = AccState.Stable;
+                }
             }
             float speedFactor = Mathf.Clamp( distance.magnitude / (stopDis*5), 0, 1);
             cur_speed_hori = cur_speed_hori * speedFactor;
-            m_transform.position += m_transform.forward * cur_speed_hori * Time.deltaTime;
-            return true;
+            cur_vel_hori = m_transform.forward * cur_speed_hori;
+            m_transform.position += cur_vel_hori * Time.deltaTime;
+        } else {
+            //cur_speed_hori = 0f; //
         }
-        return false;
     }
     private bool LocomoteVertical()
     {
@@ -146,7 +177,7 @@ public class HeliTranslate : MonoBehaviour
 
         distance.x = 0;
         distance.z = 0;
-        if (Mathf.Abs(distance.y) > 0)
+        if (Mathf.Abs(distance.y) > 1f)
         {
 
             if ((cur_speed_ver < move_speed_ver)&&(distance.y > 0f))
