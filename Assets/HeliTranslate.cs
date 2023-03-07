5038546d6de7d017ee3d05e213ee9949abe61fcd
iota11 <falconhe511@gmail.com>
2023-03-07 16:59:19 -0500

    support upside down, stop more naturally

--- Assets/HeliTranslate.cs
+++ Assets/HeliTranslate.cs
@@ -39,17 +39,20 @@ public class HeliTranslate : MonoBehaviour
     private FastNoise _fastNoise;
     private float collectedTime;
     public float cur_speed_hori = 0.0f;
-    public Vector3 cur_vel_hori;
+    public Vector3 cur_vel_hori = new Vector3(0, 0, 0);
     public float cur_speed_ver = 0.0f;
-    public Vector3 cur_vel_ver;
+    public Vector3 cur_vel_ver = new Vector3(0,0,0);
     public float cur_rotateDeg = 0.0f;
     public float cur_rotateDeg2 = 0.0f;
     public float playSpeed = 0f;
 
     public float last_speed_hori = 0.0f;
     public float last_speed_ver = 0.0f;
+    public float up_force = 10;
     public Vector3 accRotate;
     private Quaternion initial_body_rotation;
+    private float layer_weight = 0.0f;
+
 
     void Start()
     {
@@ -62,16 +65,57 @@ public class HeliTranslate : MonoBehaviour
     }
     void FixedUpdate()
     {
+        CheckState();
+        switch (m_flyState) {
+            case FlyState.Stay:
+                FlyNormal();
+                break;
+            case FlyState.Normal:
+                FlyNormal();
+                break;
+            case FlyState.Drop:
+                FlyNormal();
+                break;
+            case FlyState.Climb:
+                FlyNormal();
+                break;
+        }
+       
+
+    }
+
+    private void CheckState() {
+        Vector3 dir = des_transfrom.position - m_transform.position;
+        float distance = dir.magnitude;
+        //Debug.Log("dir is " + dir);
+        float angle = Mathf.Abs(Mathf.Acos(Vector3.Dot(dir.normalized, new Vector3(0, 1, 0))) * Mathf.Rad2Deg);
+       // Debug.Log("angle is " + angle);
+        if ((0f < angle)&& (angle <30f) && distance > 10) {
+            m_flyState = FlyState.Climb;
+        }else if((30f<angle) && (150> angle) && (distance > stopDis)) {
+            m_flyState = FlyState.Normal;
+        }else if((angle> 150f) &&(distance > 20f)) {
+            m_flyState = FlyState.Drop;
+        }else if(distance <= stopDis) {
+            m_flyState = FlyState.Stay;
+        } else {
+            m_flyState = FlyState.Normal;
+        }
+
+    }
+
+    private void Stay() {
+
+    }
+    private void FlyNormal() {
+
         LocomoteHorizontal();
         LocomoteVertical();
         Veer();
         //ApplyNoise();
-
         CalAndUpdateForce();
         ApplyAccToBody();
-
     }
-   
     private void ApplyAccToBody()
     {
         
@@ -91,11 +135,11 @@ public class HeliTranslate : MonoBehaviour
             cur_rotateDeg = Mathf.Lerp(cur_rotateDeg, rotateDeg, 0.2f);
         }
         //cur_rotateDeg = rotateDeg;
-        Quaternion detlaRotationX = Quaternion.AngleAxis(cur_rotateDeg, new Vector3(1,0,0));
+        Quaternion detlaRotationX = Quaternion.AngleAxis(cur_rotateDeg * 0.7f, new Vector3(1,0,0));
 
 
         //veer acc rotation
-        float rotateDeg2 = Mathf.Clamp(accRotate.magnitude*-5 * acc_sensitive, -65, 65);
+        float rotateDeg2 = Mathf.Clamp(accRotate.magnitude*-7 * acc_sensitive, -65, 65);
         if (Vector3.Dot(accRotate, m_transform.right) < 0)
         {
             rotateDeg2 = -rotateDeg2;
@@ -108,11 +152,14 @@ public class HeliTranslate : MonoBehaviour
         body_transform.localRotation =  initial_body_rotation * detlaRotationZ * detlaRotationX;
 
         //apply playspeed
-        playSpeed = Mathf.Clamp( Mathf.Abs(Mathf.Abs(cur_rotateDeg2)*0.6f + Mathf.Abs(cur_rotateDeg)) / 80f + 1f, 0f, 10f);
+        float up_factor = Mathf.Clamp((up_force - 10f) / 30f , -0.2f, 0.5f);
+        layer_weight =Mathf.Lerp(layer_weight,  Mathf.Clamp((15 - up_force) / 30f, 0f, 1f), 5*Time.deltaTime);
+        playSpeed = Mathf.Clamp( Mathf.Abs(Mathf.Abs(cur_rotateDeg2)*0.6f + Mathf.Abs(cur_rotateDeg)) / 110f + 1f + up_factor, 0.7f, 3f);
         if(m_accState == AccState.SpeedDown) {
             playSpeed += 0.3f;
         }
         anim.SetFloat( "flySpeed", playSpeed);
+        anim.SetLayerWeight(1, layer_weight);
     }
 
     private void ApplyNoise()
@@ -145,56 +192,55 @@ public class HeliTranslate : MonoBehaviour
         {
             if (distance.magnitude < stopDis * 10f) {
                 if (cur_speed_hori > 3f) {    //slow down to 3f
-                    cur_speed_hori -= acc *3* Time.deltaTime;
+
+                    cur_speed_hori -= Mathf.Clamp(cur_speed_hori/2 , 2*acc, acc*5)* Time.deltaTime;
                     m_accState = AccState.SpeedDown;
-                    m_transform.position += new Vector3(0, 1, 0) * 4f * Time.deltaTime;
-                    Debug.Log("brace");
                 } else {
+                    cur_speed_hori = 3f;
                     m_accState = AccState.Stable;
                 }
             } else {
                 if (cur_speed_hori < move_speed_hori) { //acc to move speed max
                     cur_speed_hori += acc  * Time.deltaTime;
                     m_accState = AccState.SpeedUp;
-                    m_transform.position += new Vector3(0,1,0) *-10f * Time.deltaTime;
                 } else {
                     m_accState = AccState.Stable;
                 }
             }
-            float speedFactor = Mathf.Clamp( distance.magnitude / (stopDis*5), 0, 1);
-            cur_speed_hori = cur_speed_hori * speedFactor;
+           //float speedFactor = Mathf.Clamp( distance.magnitude / (stopDis*5), 0, 1);
+           //cur_speed_hori = cur_speed_hori * speedFactor;
             cur_vel_hori = m_transform.forward * cur_speed_hori;
-            m_transform.position += cur_vel_hori * Time.deltaTime;
         } else {
             //cur_speed_hori = 0f; //
         }
+        m_transform.position += cur_vel_hori * Time.deltaTime;
+
     }
-    private bool LocomoteVertical()
+    private void LocomoteVertical()
     {
         Vector3 distance = des_transfrom.position - m_transform.position;
         Vector3 distance2D = new Vector3(distance.x, 0, distance.z);
         float speed_ratio =2* distance.y / distance2D.magnitude;
-
         distance.x = 0;
         distance.z = 0;
-        if (Mathf.Abs(distance.y) > 1f)
-        {
+        up_force =  10;
 
-            if ((cur_speed_ver < move_speed_ver)&&(distance.y > 0f))
+        if (Mathf.Abs(distance.y) > stopDis * 4f) //acc
+        {
+            if ((cur_speed_ver < move_speed_ver)||(Vector3.Dot(distance.normalized, cur_vel_ver) < 0f)) 
             {
-                cur_speed_ver += acc * Time.deltaTime;
-                //force += acc * m_transform.up;
+                cur_vel_ver += distance.normalized * acc * Time.deltaTime;
+                up_force = distance.normalized.y * acc +10;
             }
-            if (distance.y < 0f)
-            {
-                cur_speed_ver += 10.0f * Time.deltaTime;
+        } else {
+            if ((cur_speed_ver > 2f) ) {
+                cur_vel_ver -= cur_vel_ver.normalized * Mathf.Clamp(cur_vel_ver.magnitude, 0, 4f)*acc * Time.deltaTime;
+                up_force  = 10f -cur_vel_ver.normalized.y * Mathf.Clamp(cur_vel_ver.magnitude, 0, 4f) * acc;
             }
-            float speedFactor = Mathf.Clamp(distance.magnitude / (stopDis * 5), 0.0f, 1);
-            
-            m_transform.position += distance.normalized * cur_speed_ver* speedFactor * Time.deltaTime;
-            return true;
         }
-        return false;
+        cur_speed_ver = cur_vel_ver.magnitude;
+        m_transform.position += cur_vel_ver * Time.deltaTime;
+
     }
 
     private void Veer()
@@ -203,14 +249,14 @@ public class HeliTranslate : MonoBehaviour
         distance.y = 0;
         if (distance.magnitude > stopDis)
         {
-            Vector3 targetDir = (des_transfrom.position - m_transform.position).normalized;
+            Vector3 targetDir = des_transfrom.position - m_transform.position;
             targetDir.y = 0;
-            float angle = Mathf.Acos(Vector3.Dot(targetDir, m_transform.forward));
+            float angle = Mathf.Acos(Vector3.Dot(targetDir.normalized, m_transform.forward));
             angle = Mathf.Rad2Deg * angle;
-            Vector3 veerAxis = Vector3.Cross(targetDir, m_transform.forward);
+            Vector3 veerAxis = Vector3.Cross(targetDir.normalized, m_transform.forward);
             Vector3 newVelDir =Quaternion.AngleAxis( - angle * Time.deltaTime * rotate_speed, veerAxis) * m_transform.forward;
             accRotate = (newVelDir.normalized - m_transform.forward) * cur_speed_hori;
-            if (Mathf.Abs(angle) > 0.1f) {
+            if (Mathf.Abs(angle) > 5f) {
                 m_transform.RotateAround(m_transform.position, veerAxis, -angle * Time.deltaTime * rotate_speed);
             }
         }
